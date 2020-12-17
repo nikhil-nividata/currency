569fe1feb76cc64f05d4941cfd5ad0d6590f8319
Nikhil B Jugdan <nikhiljrn24@gmail.com>
2020-12-17 12:54:59 +0530

    Added Colors and Changed layout

--- src/components/charts/index.jsx
+++ src/components/charts/index.jsx
@@ -1,106 +1,152 @@
-import React, { Component } from 'react'
-import Chart from 'chart.js';
-import styles from './index.module.css'
+import React, { Component } from "react";
+import Chart from "chart.js";
+import styles from "./index.module.css";
+import { Card } from "react-materialize";
 
 export default class index extends Component {
-    constructor(props) {
-        super(props)
-        const { currencyExchangeData } = this.props
-        const currs = new Map()
-        currs.set(1, [])
-        currs.set(2, [])
-        currs.set(3, [])
-        currs.set(4, [])
-        const rates = new Map()
-        rates.set(1, [])
-        rates.set(2, [])
-        rates.set(3, [])
-        rates.set(4, [])
-        for (const cur in currencyExchangeData.rates) {
-            if (currencyExchangeData.rates[cur] < 40) {
-                currs.get(1).push(cur)
-                rates.get(1).push(currencyExchangeData.rates[cur])
-            } else if (currencyExchangeData.rates[cur] < 100) {
-                currs.get(2).push(cur)
-                rates.get(2).push(currencyExchangeData.rates[cur])
-            } else if (currencyExchangeData.rates[cur] < 300) {
-                currs.get(3).push(cur)
-                rates.get(3).push(currencyExchangeData.rates[cur])
-            } else {
-                currs.get(4).push(cur)
-                rates.get(4).push(currencyExchangeData.rates[cur])
-            }
-        }
-        this.state = {
-            currencyExchangeData,
-            currs,
-            rates
-        }
+  constructor(props) {
+    super(props);
+    const { currencyExchangeData } = this.props;
+    const currs = new Map();
+    currs.set(1, []);
+    currs.set(2, []);
+    currs.set(3, []);
+    currs.set(4, []);
+    const rates = new Map();
+    rates.set(1, []);
+    rates.set(2, []);
+    rates.set(3, []);
+    rates.set(4, []);
+    for (const cur in currencyExchangeData.rates) {
+      if (currencyExchangeData.rates[cur] < 40) {
+        currs.get(1).push(cur);
+        rates.get(1).push(currencyExchangeData.rates[cur]);
+      } else if (currencyExchangeData.rates[cur] < 100) {
+        currs.get(2).push(cur);
+        rates.get(2).push(currencyExchangeData.rates[cur]);
+      } else if (currencyExchangeData.rates[cur] < 300) {
+        currs.get(3).push(cur);
+        rates.get(3).push(currencyExchangeData.rates[cur]);
+      } else {
+        currs.get(4).push(cur);
+        rates.get(4).push(currencyExchangeData.rates[cur]);
+      }
     }
+    this.state = {
+      currencyExchangeData,
+      currs,
+      rates,
+    };
+  }
 
+  drawChart() {
+    const { currencyExchangeData, currs, rates } = this.state;
+    console.log(currs);
+    console.log(rates);
+    console.log(rates.get(1));
+    for (let i = 1; i < 5; ++i)
+      new Chart(document.getElementById("comparisionChart" + i), {
+        type: "bar",
+        data: {
+          labels: currs.get(i),
+          datasets: [
+            {
+              label: "relative to USD",
+              data: rates.get(i),
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(240, 248, 255,0.6)",
+                "rgba(127, 255, 212,0.3)",
+                "rgba(255, 235, 205,0.2)",
+                "rgba(0, 255, 255,0.2)",
+                "rgba(255, 248, 220,0.6)",
+                "rgba(255, 0, 255, 0.2)",
+                "rgba(255, 215, 0, 0.25)",
+                "rgba(124, 252, 0, 0.25)",
+                "rgba(255, 160, 122, 0.2)",
+                "rgba(250, 50, 230, 0.2)",
+                "rgba(221, 160, 221, 0.2)",
+                "rgba(176, 224, 230, 0.2)",
+                "rgba(70, 130, 180, 0.2)",
+                "rgba(192, 192, 192, 0.2)",
+                "rgba(216, 191, 216, 0.3)",
+                "rgba(100, 58, 58, 0.2)",
+                "rgba(0, 255, 0, 0.2)",
+                "rgba(25, 250, 240, 0.2)",
+                "rgba(105, 105, 105, 0.3)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+                "rgb(0, 0, 0,.15)",
+                "aquamarine",
+                "blanchedalmond",
+                "cyan",
+                "rgb(0, 0, 0,.15)",
+                "magenta",
+                "gold",
+                "lawngreen",
+                "lightsalmon",
+                "rgb(250, 50, 230, 1)",
+                "plum",
+                "powderblue",
+                "steelblue",
+                "silver",
+                "thistle",
+                "rgba(0, 0, 0,.15)",
+                "lime",
+                "rgba(0, 0, 0,.15)",
+                "dimgrey",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      });
+  }
 
-    drawChart() {
-        const { currencyExchangeData, currs, rates } = this.state
-        console.log(currs);
-        console.log(rates);
-        console.log(rates.get(1));
-        for (let i = 1; i < 5; ++i)
-            new Chart(
-                document.getElementById('comparisionChart' + i),
-                {
-                    type: 'bar',
-                    data: {
-                        labels: currs.get(i),
-                        datasets: [{
-                            label: 'relative to USD',
-                            data: rates.get(i),
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                                'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)',
-                                'rgba(255, 159, 64, 0.2)'
-                            ],
-                            borderColor: [
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)',
-                                'rgba(255, 159, 64, 1)'
-                            ],
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }]
-                        }
-                    }
-                }
-            )
-    }
+  componentDidMount() {
+    this.drawChart();
+  }
 
-    componentDidMount() {
-        this.drawChart()
-    }
-
-    render() {
-        return (
-            <div>
-                <div className={styles.chartContainer}>
-
-                    <canvas id="comparisionChart1" width="400" height="160"></canvas>
-                    <canvas id="comparisionChart2" width="400" height="160"></canvas>
-                    <canvas id="comparisionChart3" width="400" height="160"></canvas>
-                    <canvas id="comparisionChart4" width="400" height="160"></canvas>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div className={styles.chartContainer}>
+          <Card className="container">
+            <canvas id="comparisionChart1" width="400" height="160" />
+          </Card>
+          <Card className="container">
+            <canvas id="comparisionChart2" width="400" height="160" />
+          </Card>
+          <Card className="container">
+            <canvas id="comparisionChart3" width="400" height="160" />
+          </Card>
+          <Card className="container">
+            <canvas id="comparisionChart4" width="400" height="160" />
+          </Card>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/common/navbar/index.jsx
+++ src/components/common/navbar/index.jsx
@@ -1,53 +1,53 @@
-import React from 'react'
-import { Button, Icon, Navbar, NavItem } from 'react-materialize'
-import { Link } from 'react-router-dom'
-import styles from './index.module.css'
+import React from "react";
+import { Button, Icon, Navbar, NavItem } from "react-materialize";
+import { Link } from "react-router-dom";
+import styles from "./index.module.css";
 
 export default function index() {
-    return (
-        <div>
-            <Navbar
-                menuIcon={null}
-                alignLinks="right"
-                brand={
-                    <div className={"brand-logo " + styles.brandLogoHolder}>
-                        <Icon
-                            style={{ fontSize: '2.2rem', cursor: 'pointer' }}
-                            onClick={
-                                () => {
-                                    document.getElementById('sideNavToggler').click()
-                                }
-                            }
-                        >
-                            menu
-                        </Icon>
-                        <span>Foriegn Exchange</span>
-                    </div>
-                }
-                centerLogo={false}
-                id="full-nav"
-                options={{
-                    draggable: true,
-                    edge: 'left',
-                    inDuration: 250,
-                    onCloseEnd: null,
-                    onCloseStart: null,
-                    onOpenEnd: null,
-                    onOpenStart: null,
-                    outDuration: 200,
-                    preventScrolling: true
-                }}
+  return (
+    <div>
+      <Navbar
+        menuIcon={null}
+        alignLinks="right"
+        brand={
+          <div className={"brand-logo " + styles.brandLogoHolder}>
+            <Icon
+              style={{ fontSize: "2.2rem", cursor: "pointer" }}
+              onClick={() => {
+                document.getElementById("sideNavToggler").click();
+              }}
             >
-                <Link style={{ fontSize: '1.6rem' }} to="/">
-                    Home
-                    </Link>
-                <Link style={{ fontSize: '1.6rem' }} to="/chart">
-                    Compare Currencies
-                    </Link>
-                <Link style={{ fontSize: '1.6rem', marginRight: '2vw' }} to="/history">
-                    History
-                </Link>
-            </Navbar>
-        </div>
-    )
+              menu
+            </Icon>
+            <Link to="/">
+              <span>Foriegn Exchange</span>
+            </Link>
+          </div>
+        }
+        centerLogo={false}
+        id="full-nav"
+        options={{
+          draggable: true,
+          edge: "left",
+          inDuration: 250,
+          onCloseEnd: null,
+          onCloseStart: null,
+          onOpenEnd: null,
+          onOpenStart: null,
+          outDuration: 200,
+          preventScrolling: true,
+        }}
+      >
+        <Link style={{ fontSize: "1.6rem" }} to="/">
+          Home
+        </Link>
+        <Link style={{ fontSize: "1.6rem" }} to="/chart">
+          Compare Currencies
+        </Link>
+        <Link style={{ fontSize: "1.6rem", marginRight: "2vw" }} to="/history">
+          History
+        </Link>
+      </Navbar>
+    </div>
+  );
 }
